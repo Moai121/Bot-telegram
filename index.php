22844b0d79a0ad49b85b1f7bb9c01fef53024cc2
Fermín Vílchez López <fvl0001@alu.medac.es>
2022-02-10 15:25:47 +0100

    ok

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php 
-    echo("Hola");
+<?php
+$token = "5261731576:AAGdSJRelD3k5Hl-Wnkudj1lPdm3W1mVSa4";
+$website = 'https://api.telegram.org/bot'.$token;
+
+$input = file_get_contents('php://input');
+$update = json_decode($input, TRUE);
+
+$chatId = $update['message']['chat']['id'];
+$message = $update['message']['text'];
+
+switch($message) {
+    case '/start':
+        $response = 'Me has iniciado';
+        sendMessage($chatId, $response);
+        break;
+    case '/info':
+        $response = 'Hola! Soy @Botnoticiero';
+        sendMessage($chatId, $response);
+        break;
+    default:
+        $response = 'No te he entendido';
+        sendMessage($chatId, $response);
+        break;
+}
+
+function sendMessage($chatId, $response) {
+    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    file_get_contents($url);
+}
 ?>

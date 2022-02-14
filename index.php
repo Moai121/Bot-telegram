e858d5dcea4539c9101ee5b559aac335e69398a0
Fermín Vílchez López <fvl0001@alu.medac.es>
2022-02-14 18:52:54 +0100

    ok

--- index.php
+++ index.php
@@ -17,9 +17,8 @@ switch($message) {
         $response = 'Hola! Soy @Botnoticiero';
         sendMessage($chatId, $response);
         break;
-    case '/partidos':
-        $response  = 'Los partidos de hoy son:';
-        sendMessage($chatId,$response);
+    case '/noticias':
+        mostrarnoticias($chatId);
         break;
     default:
         $response = 'No te he entendido';
@@ -30,5 +29,24 @@ switch($message) {
 function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
+}
+// function buscarnoticia($chatId,$destino,$inicio,$localicacion){
+//     $tokengoogle="AIzaSyAkr2AnND93qyJcRn1TDgR_UwWPdmjQoiA";
+//     $url="https://maps.googleapis.com/maps/api/directions/json?origin=".urlencode($inicio)."&destination=".urlencode($destino)."&key=".$tokengoogle;
+//     $resultado=file_get_contents($url);
+//     $resultado=json_decode($resultado,TRUE);
+// }
+function mostrarnoticias($chatId,$noticia){
+    $context=stream_context_create(array('http'=>array('header'=>'Accept:application/xml')));
+    $url="http://www.europapress.es/rss/rss.aspx";
+    $xmlsrting=file_get_contents($url,false,$context);
+    $xml=simplexml_load_string($xmlsrting,"SimpleXMLElement",LIBXML_NOCDATA);
+    $json=json_encode($xml);
+    $array=json_decode($json,TRUE); 
+
+    for($i=0;$i <9;$i++){
+        $titulo=$titulo."\n\n".$array['channel']['item'][$i]['title']."<a href='>".$array["channel"]["item"][$i]["link"]."'>+info</a> ";
+    }
+    sendMessage($chatId,$titulo);
 }
 ?>

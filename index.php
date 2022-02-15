d0a5ad8856a15009052a3ee4bc2902e1a42772ee
Fermín Vílchez López <fvl0001@alu.medac.es>
2022-02-15 20:42:09 +0100

    ok

--- index.php
+++ index.php
@@ -31,12 +31,12 @@ switch($message) {
 }
 
 function sendMessage($chatId, $response,$repl) {
-    if($repl==TRUE){
-        $reply_mark=array('force_reply'=>True);
-        $url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($responde);
-    }
-    else
-    $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    if ($repl == TRUE){ 
+        $reply_mark = array('force_reply' => True); 
+        $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response); 
+    }else{ 
+        $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response); 
+    } 
     file_get_contents($url);
 }
 function mostrarnoticias($chatId){

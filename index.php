a865dfd58a3674b645571a6dddd8a0765f756992
Fermín Vílchez López <fvl0001@alu.medac.es>
2022-02-16 16:06:31 +0100

    ok

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ function sendMessage($chatId, $response,$repl) {
 }
 function mostrarnoticias($chatId){
     $context=stream_context_create(array('http'=>array('header'=>'Accept:application/xml')));
-    $url="https://www.europapress.es/rss/rss.aspx";
+    $url="https://www.elperiodico.com/es/rss/rss_portada.xml";
     $xmlsrting=file_get_contents($url,false,$context);
     $xml=simplexml_load_string($xmlsrting,"SimpleXMLElement",LIBXML_NOCDATA);
     $json=json_encode($xml);
@@ -55,7 +55,13 @@ function mostrarnoticias($chatId){
 function elegircategoria($chatId,$response,$repl){
     if ($repl == TRUE){ 
         $reply_mark = array('force_reply' => True); 
-        $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response); 
+        $url = "https://www.elperiodico.com/es/rss/economia/rss.xml"; 
+        $xmlsrting=file_get_contents($url,false,$context);
+        $xml=simplexml_load_string($xmlsrting,"SimpleXMLElement",LIBXML_NOCDARA);
+        $json=json_encode($xml);
+        $array=json_encode($json,TRUE);
+        $categoria="Seleccione que categoria desea";
+        sendMessage($chatId,$categoria,TRUE);
     }else{ 
         $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response); 
     } 
